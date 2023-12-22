6680365d12cd2ddee4cd5f92a0074e1f956e0500
Atif Hameed <atifhameed11312@gmail.com>
2023-12-22 23:42:39 +0500

    more patterns

--- src/Patterns.java
+++ src/Patterns.java
@@ -49,12 +49,63 @@ public class Patterns {
     }
 
 
-    public void reverseTriangle
+    public void reverseTriangle(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows : ");
+        int r = sc.nextInt();
+        for(int i=r; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
+
+    public void rightTriange(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows : ");
+        int r = sc.nextInt();
+        for (int i=1; i<=r; i++){
+            for(int j=1; j<= r-i; j++){
+                System.out.print(" ");
+            }
+            for (int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public void numberTriangle(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows : ");
+        int r = sc.nextInt();
+        for(int i=1; i<=r; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+
+    public void reverseNmberTrianlge(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rows : ");
+        int r = sc.nextInt();
+        for(int i=r; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args){
         Patterns myObj = new Patterns();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your choice 1-Rectangle / 2-EmptyRectangle / 3-Triangle: ");
+        System.out.print("Enter your choice : \n1-Rectangle : \n2-EmptyRectangle : \n3-Triangle : \n4-ReverseTrianle : \n5-rightTriangle : \n6-numberTriangle : \n7-reverseNumberTriangle :");
         int n = sc.nextInt();
         switch (n){
             case 1 :{
@@ -67,6 +118,22 @@ public class Patterns {
             }
             case 3 :{
                 myObj.triangle();
+                break;
+            }
+            case 4 :{
+                myObj.reverseTriangle();
+                break;
+            }
+            case 5 :{
+                myObj.rightTriange();
+                break;
+            }
+            case 6 :{
+                myObj.numberTriangle();
+                break;
+            }
+            case 7 :{
+                myObj.reverseNmberTrianlge();
                 break;
             }
         }

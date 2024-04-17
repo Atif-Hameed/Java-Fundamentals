0721359948c8168e0c9874c1dab9fecbd0e0d491
Atif Hameed <atifhameed11312@gmail.com>
2024-04-17 23:48:29 +0500

    learns arrays

---
+++ src/ArraysLearns.java
@@ -0,0 +1,7 @@
+public class ArraysLearns {
+    public static void main(String[] args){
+        int sampleArray[];
+        sampleArray = new int[]{1,2,3};
+        System.out.println(sampleArray[0]);
+    }
+}

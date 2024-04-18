2212b0c9836c15f76eb2fe9a37cbf2106a4b4551
Atif Hameed <atifhameed11312@gmail.com>
2024-04-18 23:47:28 +0500

    arrays

--- src/ArraysLearns.java
+++ src/ArraysLearns.java
@@ -1,7 +1,17 @@
 public class ArraysLearns {
     public static void main(String[] args){
-        int sampleArray[];
-        sampleArray = new int[]{1,2,3};
-        System.out.println(sampleArray[0]);
+        String[] myArray = {"A","T","I","F"};
+        System.out.print("[ ");
+        for (int i =0; i<4; i++){
+            System.out.print(myArray[i]+" ");
+        }
+        System.out.print("]");
+
+        int[] myArray2 = {9,8,7,6};
+        System.out.print("[ ");
+        for (int i =0; i<4; i++){
+            System.out.print(myArray2[i]+" ");
+        }
+        System.out.print("]");
     }
 }
